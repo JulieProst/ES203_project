8157970bace4bd3d8d41a960c86333fa4835b5ee
Benjamin Chabanne <benjamin.chabanne.95@gmail.com>
2017-01-05 21:31:59 +0100

    Exploitation des données

--- calculerreur.cpp
+++ calculerreur.cpp
@@ -34,17 +34,21 @@ static void chargerInfoFichiers(std::vector<fichierImage>*img)
 	info.nom="rec/rec_9.bmp"; info.nb=446; info.value=9; img->push_back(info);
 }
 
-static std::string analyseFichier(int(*f)(const Matrice&),const fichierImage& file)
+static std::string analyseFichier(int(*f)(const Matrice&),const fichierImage& file,std::vector<Matrice>*ensembleImage)
 {
 	Matrice M(readBMP(file.nom));
 	int detected[11]={0};
 	for(int i=0;i<file.nb;i++)		
 	{
 		int val=f(M.lettre(i));		
-		if(val==-1)
+		if(val==-1){
 			detected[10]++;
-		else
+			ensembleImage[10].push_back(M.lettre(i));
+		}
+		else {
 			detected[val]++;
+			ensembleImage[val].push_back(M.lettre(i));
+		}
 	}
 
 	std::cout<<"\tErreur de "<<100-detected[file.value]*100/file.nb<<"%"<<std::endl;
@@ -58,20 +62,53 @@ static std::string analyseFichier(int(*f)(const Matrice&),const fichierImage& fi
 	return str+"\n";
 }
 
+static void createImages(std::string nom,std::vector<Matrice>* ensembleImage)
+{
+	for(unsigned int i=0;i<11;i++) {
+		std::ofstream file;
+		file.open((nom+"/chiffre"+inString(i)+".pbm").c_str());
+		unsigned int h;
+
+		h=ensembleImage[i].size()/20+1;
+		file<<"P1\n"<<20*64<<" "<<h*64<<std::endl;
+
+		for(unsigned int j=0;j<h;j++) {
+			for(unsigned int ligne=0;ligne<64;ligne++) {
+				for(unsigned int a=0;a<20;a++) {
+					if(j==h-1 && j*20+a>=ensembleImage[i].size())
+						for(unsigned int colonne=0;colonne<64;colonne++) 
+							file<<0<<" ";
+					else 
+						for(unsigned int colonne=0;colonne<64;colonne++) 
+							file<<1-ensembleImage[i][j*20+a](ligne,colonne)<<" ";
+				}
+				file<<std::endl;
+			}
+		}
+
+
+		file.close();
+	}
+}
+
 void calculErreur(int(*f)(const Matrice&),const std::string& nom)
 {
 	std::vector<fichierImage>img;
 	chargerInfoFichiers(&img);
 
+	std::vector<Matrice> ensembleImage[11];
+
 	std::ofstream file;
-	file.open(nom+"/resultats.csv");
-	file<<"Nombre étudié,0,1,2,3,4,5,6,7,8,9,fauxpositifs\n";
+	file.open((nom+"/resultats.csv").c_str());
+	file<<"Nombre étudié,0,1,2,3,4,5,6,7,8,9,illisibles\n";
 
 	for(unsigned int i=0;i<img.size();i++)
 	{
 		std::cout<<"Analyse de "<<img[i].nom<<std::endl;
-		file << analyseFichier(f,img[i]);
+		file << analyseFichier(f,img[i],ensembleImage);
 	}
+	std::cout<<"Ecriture des donnees."<<std::endl;
+	createImages(nom,ensembleImage);
 	file.close();
 }
 
