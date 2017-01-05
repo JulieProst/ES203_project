e633a0cf90ea0b5dbf0626cd874e00c9f42b3cd7
jvalenchon <valenchon.juliette@gmail.com>
2017-01-05 16:48:32 +0100

    le cpp

---
+++ characteristique.cpp
@@ -0,0 +1,25 @@
+#include "characteristique.h"
+
+
+int perimetre(vecteur vec){
+    int i=0;
+    if (vec(i) ==0) {
+        i++;
+    }
+    int pos_in=i;
+
+
+        }
+
+
+int surface(Matrice matr){
+    int surf=0;
+     for (int i=0; i<62, i++){
+        for (int j=0; j<62, j++){
+                if matr(i,j)==0{
+                    surf ++;
+                }
+    }
+    }
+    return surf;
+}
