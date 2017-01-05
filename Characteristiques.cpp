676528c4ca1f0308fb1e2c7a6fc3d23146457ca4
JulieProst <julie.prost95@gmail.com>
2017-01-05 17:40:03 +0100

    Fichier qui calcule les caracteristiques des chif

--- Characteristiques.cpp
+++ Characteristiques.cpp
@@ -1,59 +1,60 @@
 #include <iostream>
-
 #include "matrice.h"
+#include "Characteristiques.h"
+#include <vector>
 
 using namespace std;
 
+// Calcule le nombre de voisins blancs de l'élément (x,y) de la matrice
+// Ne fonctionne pas pour les éléments du bord
+int nbr_voisins_blanc(Matrice matrice, int x, int y)
+{
+    int nbr_voisins = 0;
+    if (matrice(x-1,y)==1){nbr_voisins++;}
+    if (matrice(x,y-1)==1){nbr_voisins++;}
+    if (matrice(x+1,y)==1){nbr_voisins++;}
+    if (matrice(x,y+1)==1){nbr_voisins++;}
+    return nbr_voisins;
+}
+
 int perimeter(Matrice matrice)
 {
-    int perimetre = 1;
-    int i=0, j=0;
+    int perimetre = 0;
+    int nbr_vois = 0;
+    vector<Couple> Index;
 
-    // On cherche le premier pixel noir
-    int I_in = 0;
-    int J_in = 0;
-    while (matrice(I_in,J_in) = 1)
-    {
-        if J_in < 62
-        {
-            J_in++;
-        }
-        else
-        {
-            J_in = 0;
-            I_in++;
-        }
-
-        if I_in >= 62
-        {
-            cout << "Error : l'image est entièrement blanche" << endl;
-            return 0;
+    // On recense les coordonnees de tous les pixels noirs
+    for(int i = 0; i<64; i++){
+        for(int j = 0; j<64; j++){
+            if(matrice(i,j)==0){
+                Couple couple;
+                couple.i=i;
+                couple.j=j;
+                Index.push_back(couple);
+            }
         }
     }
 
-    // On cherche le premier voisin
-    if (matrice(I_in-1,J_in) = 1)
-    {
-        perimetre++;
-        i = I_in-1;
-        j = J_in+1;
+    // On compte les voisins blancs de chaque pixel noir
+    for(int k = 0; k<Index.size(); k++){
+        int i = Index[k].i;
+        int j = Index[k].j;
+        nbr_vois = nbr_voisins_blanc(matrice,i,j);
+        perimetre+=nbr_vois;
     }
-
-    else if (matrice(I_in-1,J_in) = 1 && matrice(I_in))
-    {
-
-    }
-
-    // On lance la boucle qui calcule aussi le perimètre
-    while (i!=I_in && j!=J_in  )
-        {
-            if (matrice(i+1,j) = 1
-                {
-                    perimetre++;
-                }
-            if (matrice())
-        }
-
 
     return perimetre;
 }
+
+int surface(Matrice matr){
+    int surf=0;
+     for (int i=0; i<62; i++){
+        for (int j=0; j<62; j++){
+                if (matr(i,j)==0){
+                    surf ++;
+                }
+    }
+    }
+    return surf;
+}
+
---
+++ Characteristiques.h
@@ -0,0 +1,10 @@
+#ifndef CHARACTERISTIQUES
+#define CHARACTERISTIQUES
+
+int nbr_voisins_blanc(Matrice matrice, int x, int y);
+int perimeter(Matrice matrice);
+int surface(Matrice matr);
+
+struct Couple{int i; int j;};
+
+#endif // CHARACTERISTIQUES
